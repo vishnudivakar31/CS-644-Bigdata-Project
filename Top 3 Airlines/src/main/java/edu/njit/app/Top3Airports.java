4559468cd6338fa55af6a870178ae1dbe863eba9
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-04-16 20:34:04 -0400

    top 3 airports

--- Top 3 Airlines/src/main/java/edu/njit/app/Top3Airports.java
+++ Top 3 Airlines/src/main/java/edu/njit/app/Top3Airports.java
@@ -1,9 +1,8 @@
 package edu.njit.app;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -12,87 +11,89 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.text.DecimalFormat;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Top3Airports {
 
-    private static final Integer ORIGIN_INDEX = 16, DEST_INDEX = 17, TAXI_IN_INDEX = 19, TAXI_OUT_INDEX = 20;
-    private static Map<String, String> airportCodeMap = new HashMap<>();
+    private static final Integer ORIGIN_INDEX = 16, DEST_INDEX = 17, TAXI_OUT_INDEX = 20, TAXI_IN_INDEX = 19;
 
-    public static class AirportMapper extends Mapper<LongWritable, Text, Text, Text> {
-
+    public static class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String[] rawData = value.toString().split(",");
-            try {
-                if(rawData.length == 29) {
-                    Integer taxiInTime = Integer.parseInt(rawData[TAXI_IN_INDEX]);
-                    Integer taxiOutTime = Integer.parseInt(rawData[TAXI_OUT_INDEX]);
-                    String originCode = rawData[ORIGIN_INDEX];
-                    String destCode = rawData[DEST_INDEX];
-                    context.write(new Text(originCode), new Text(String.format("%d,1", taxiOutTime)));
-                    context.write(new Text(destCode), new Text(String.format("%d,1", taxiInTime)));
+            if(!rawData[0].equalsIgnoreCase("year")) {
+                String origin = rawData[ORIGIN_INDEX].trim();
+                String dest = rawData[DEST_INDEX].trim();
+                try {
+                    int taxiOutTime = Integer.parseInt(rawData[TAXI_OUT_INDEX]);
+                    int taxiInTime = Integer.parseInt(rawData[TAXI_IN_INDEX]);
+                    context.write(new Text(origin), new IntWritable(taxiOutTime));
+                    context.write(new Text(dest), new IntWritable(taxiInTime));
+                } catch (NumberFormatException e) {
+                    System.out.println(e.getMessage());
                 }
-            } catch (NumberFormatException e) {
-                System.out.println(e.getMessage());
             }
         }
     }
 
-    public static class AirportReducer extends Reducer<Text, Text, Text, DoubleWritable> {
-
-        Map<Text, Double> result;
+    public static class AirportReducer extends Reducer<Text, IntWritable, Text, Text> {
+        private Map<String, Double> resultMap;
 
         @Override
         protected void setup(Context context) {
-            result = new HashMap<>();
+            resultMap = new HashMap<>();
         }
 
         @Override
-        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-            super.reduce(key, values, context);
-            int totalCount = 0, totalTaxiTime = 0;
-            for(Text value : values) {
-                String[] data = value.toString().split(",");
-                int taxiTime = Integer.parseInt(data[0]);
-                int count = Integer.parseInt(data[1]);
-                totalTaxiTime += taxiTime;
-                totalCount += count;
+        protected void reduce(Text key, Iterable<IntWritable> values, Context context) {
+            int count = 0, sum = 0;
+            for(IntWritable value : values) {
+                sum += value.get();
+                count++;
             }
-            result.put(key, ((double) totalTaxiTime / (double) totalCount));
+            Double avgTaxiTime = (double) (sum) / (double) (count);
+            resultMap.put(key.toString(), avgTaxiTime);
         }
 
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
-            Map<Text, Double> airportByShortestTaxiTime = new HashMap<>();
-            Map<Text, Double> airportByLongestTaxiTime = new HashMap<>();
+            Map<String, Double> shortestTaxi;
+            Map<String, Double> longestTaxi;
+            DecimalFormat df = new DecimalFormat("0.0000");
 
-            result.entrySet()
+            shortestTaxi = resultMap.entrySet()
                     .stream()
                     .sorted(Map.Entry.comparingByValue())
-                    .forEachOrdered(item -> airportByShortestTaxiTime.put(item.getKey(), item.getValue()));
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                            (v1, v2) -> v1, LinkedHashMap::new));
 
-            result.entrySet()
+            longestTaxi = resultMap.entrySet()
                     .stream()
                     .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                    .forEachOrdered(item -> airportByLongestTaxiTime.put(item.getKey(), item.getValue()));
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                            (v1, v2) -> v1, LinkedHashMap::new));
 
             int counter = 0;
-            for(Map.Entry<Text, Double> airport : airportByLongestTaxiTime.entrySet()) {
-                String key = airport.getKey().toString();
-                context.write(new Text(airportCodeMap.get(key)), new DoubleWritable(airport.getValue()));
+            context.write(new Text("Total airport entries"), new Text(String.valueOf(longestTaxi.entrySet().size())));
+            context.write(new Text("Airports with Longest Average Taxi Time"), new Text("Average Time in Minutes"));
+            for(Map.Entry<String, Double> entry : longestTaxi.entrySet()) {
+                String value = df.format(entry.getValue());
+                context.write(new Text(entry.getKey()), new Text(value));
                 counter++;
                 if(counter == 3) break;
             }
+
             counter = 0;
-            for(Map.Entry<Text, Double> airport : airportByShortestTaxiTime.entrySet()) {
-                String key = airport.getKey().toString();
-                context.write(new Text(airportCodeMap.get(key)), new DoubleWritable(airport.getValue()));
+            context.write(new Text("Airports with Shortest Average Taxi Time"), new Text("Average Time in Minutes"));
+            for(Map.Entry<String, Double> entry : shortestTaxi.entrySet()) {
+                String value = df.format(entry.getValue());
+                context.write(new Text(entry.getKey()), new Text(value));
                 counter++;
                 if(counter == 3) break;
             }
@@ -101,24 +102,17 @@ public class Top3Airports {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Configuration configuration = new Configuration();
-        if(args.length < 3) {
-            System.out.println("Please provide an input file, an output directory and airport code file");
-            System.exit(-1);
-        }
-
-        loadAirportCodeMap(args[2], configuration);
-
-        Job job = Job.getInstance(configuration, "Top 3 Airports");
+        Job job = Job.getInstance(configuration, "top 3 airports");
         job.setJarByClass(Top3Airports.class);
 
         job.setMapperClass(AirportMapper.class);
         job.setReducerClass(AirportReducer.class);
 
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
+        job.setMapOutputValueClass(IntWritable.class);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(DoubleWritable.class);
+        job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -126,19 +120,4 @@ public class Top3Airports {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
-    private static void loadAirportCodeMap(String airportFilePath, Configuration configuration) throws IOException {
-        String line;
-        Path path = new Path(airportFilePath);
-        FileSystem fileSystem = FileSystem.get(configuration);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fileSystem.open(path)));
-        while((line = bufferedReader.readLine())!= null) {
-            String[] data = line.split(",");
-            String airportCode = data[0];
-            String airportName = data[1];
-            String city = data[2];
-            String state = data[3];
-            String country = data[4];
-            airportCodeMap.put(airportCode, String.format("%s- %s, %s, %s", airportName, city, state, country));
-        }
-    }
 }
