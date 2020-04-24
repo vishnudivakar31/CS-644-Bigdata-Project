8359ff8964fe97d48b3f51808d7232d3a8572dfd
Vishnu Divakar <vishnudivakar31@gmail.com>
2020-04-23 23:41:39 -0400

    airport code mapping to airport name

--- Top 3 Airlines/src/main/java/edu/njit/app/Top3Airports.java
+++ Top 3 Airlines/src/main/java/edu/njit/app/Top3Airports.java
@@ -1,6 +1,7 @@
 package edu.njit.app;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -11,8 +12,11 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.text.DecimalFormat;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -22,6 +26,8 @@ import java.util.stream.Collectors;
 public class Top3Airports {
 
     private static final Integer ORIGIN_INDEX = 16, DEST_INDEX = 17, TAXI_OUT_INDEX = 20, TAXI_IN_INDEX = 19;
+    private static final Integer CODE_INDEX = 0, NAME_INDEX = 1, CITY_INDEX = 2, STATE_INDEX = 3, COUNTRY_INDEX = 4;
+    private static final String AIRPORT_FILE_PATH = "airport_file";
 
     public static class AirportMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
         @Override
@@ -44,10 +50,28 @@ public class Top3Airports {
 
     public static class AirportReducer extends Reducer<Text, IntWritable, Text, Text> {
         private Map<String, Double> resultMap;
+        private Map<String, String> airportCode;
 
         @Override
-        protected void setup(Context context) {
+        protected void setup(Context context) throws IOException {
             resultMap = new HashMap<>();
+            airportCode = new HashMap<>();
+            Path path = new Path(context.getConfiguration().get(AIRPORT_FILE_PATH));
+            FileSystem fs = FileSystem.get(context.getConfiguration());
+            try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fs.open(path)))) {
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    String[] lineList = line.split(",");
+                    String code = lineList[CODE_INDEX].replaceAll("\"", "").trim();
+                    if (code.equalsIgnoreCase("iata")) continue;
+                    String name = lineList[NAME_INDEX].replaceAll("\"", "").trim();
+                    String city = lineList[CITY_INDEX].replaceAll("\"", "").trim();
+                    String state = lineList[STATE_INDEX].replaceAll("\"", "").trim();
+                    String country = lineList[COUNTRY_INDEX].replaceAll("\"", "").trim();
+                    String airportIdentifier = String.format("%s, %s %s, %s", name, city, state, country);
+                    airportCode.put(code, airportIdentifier);
+                }
+            }
         }
 
         @Override
@@ -57,15 +81,16 @@ public class Top3Airports {
                 sum += value.get();
                 count++;
             }
-            Double avgTaxiTime = (double) (sum) / (double) (count);
-            resultMap.put(key.toString(), avgTaxiTime);
+            if(sum > 0 && count > 0) {
+                Double avgTaxiTime = (double) (sum) / (double) (count);
+                resultMap.put(key.toString(), avgTaxiTime);
+            }
         }
 
         @Override
         protected void cleanup(Context context) throws IOException, InterruptedException {
             Map<String, Double> shortestTaxi;
             Map<String, Double> longestTaxi;
-            DecimalFormat df = new DecimalFormat("0.0000");
 
             shortestTaxi = resultMap.entrySet()
                     .stream()
@@ -83,8 +108,14 @@ public class Top3Airports {
             context.write(new Text("Total airport entries"), new Text(String.valueOf(longestTaxi.entrySet().size())));
             context.write(new Text("Airports with Longest Average Taxi Time"), new Text("Average Time in Minutes"));
             for(Map.Entry<String, Double> entry : longestTaxi.entrySet()) {
-                String value = df.format(entry.getValue());
-                context.write(new Text(entry.getKey()), new Text(value));
+                BigDecimal bd = BigDecimal.valueOf(entry.getValue());
+                bd = bd.setScale(4, RoundingMode.HALF_UP);
+                String value = String.valueOf(bd.doubleValue());
+                if(airportCode.containsKey(entry.getKey())) {
+                    context.write(new Text(airportCode.get(entry.getKey())), new Text(value));
+                } else {
+                    context.write(new Text(entry.getKey()), new Text(value));
+                }
                 counter++;
                 if(counter == 3) break;
             }
@@ -92,8 +123,14 @@ public class Top3Airports {
             counter = 0;
             context.write(new Text("Airports with Shortest Average Taxi Time"), new Text("Average Time in Minutes"));
             for(Map.Entry<String, Double> entry : shortestTaxi.entrySet()) {
-                String value = df.format(entry.getValue());
-                context.write(new Text(entry.getKey()), new Text(value));
+                BigDecimal bd = BigDecimal.valueOf(entry.getValue());
+                bd = bd.setScale(4, RoundingMode.HALF_UP);
+                String value = String.valueOf(bd.doubleValue());
+                if(airportCode.containsKey(entry.getKey())) {
+                    context.write(new Text(airportCode.get(entry.getKey())), new Text(value));
+                } else {
+                    context.write(new Text(entry.getKey()), new Text(value));
+                }
                 counter++;
                 if(counter == 3) break;
             }
@@ -101,7 +138,12 @@ public class Top3Airports {
     }
 
     public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        if(args.length != 3) {
+            System.out.println("expecting 3 arguments, \n1.Input file\n2.Output Directory\n3.Airport CSV");
+            System.exit(-1);
+        }
         Configuration configuration = new Configuration();
+        configuration.set(AIRPORT_FILE_PATH, args[2]);
         Job job = Job.getInstance(configuration, "top 3 airports");
         job.setJarByClass(Top3Airports.class);
 
@@ -114,6 +156,7 @@ public class Top3Airports {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
 
+        FileInputFormat.setInputDirRecursive(job, true);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
